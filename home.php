3e1134b17b6604ff10d0963bfce0ced6ef77c781
wateryfiery <110883885+wateryfiery@users.noreply.github.com>
2022-08-24 09:25:53 +0200

    starting of home page
    will create a home page that will go to every path of the adventure

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+
+  <link rel="stylesheet" href="general.css">
+  <link rel="stylesheet" href="header.css">
+  <link rel="stylesheet" href="home.css">
+
+  <title>Home</title>
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+<p>this is <a href="">test</a></p>
+</body>
+</html>
